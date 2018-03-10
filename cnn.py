83dcd2175ea3aaeedacf78c42eefc85c1dd4ceaa
Marcel <marcelb@uvic.ca>
2018-03-09 16:50:36 -0800

    Add additonal convolutional layer and dense layer, and increase size of layer 1 and 2

--- cnn.py
+++ cnn.py
@@ -5,6 +5,7 @@ from data_preprocessing import get_input_data
 
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.metrics import accuracy_score
+from keras import optimizers
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, GlobalMaxPooling2D, Dense, Dropout, Input, Flatten, Activation
 from keras.layers.merge import Concatenate
@@ -13,32 +14,42 @@ from keras.utils import np_utils
 
 
 def get_model():
-    """ Generates and compiles a sequential model with 3 convolutional 2D layers,
-    1 dense layer and a sigmoid function."""
+    """ Generates and compiles a sequential model with 4 convolutional 2D layers,
+    2 dense layer and a sigmoid function. Initial implementation inspired by
+    https://www.kaggle.com/cbryant/keras-cnn-statoil-iceberg-lb-0-1995-now-0-1516"""
     # Sequential model
     model = Sequential()
     # Conv2D input layer
-    model.add(Convolution2D(16, (3, 3), strides = (1,1), activation = 'relu', input_shape = (75, 75, 2)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Convolution2D(64, (3, 3), strides = (1,1), activation = 'relu', input_shape = (75, 75, 2)))
+    model.add(MaxPooling2D(pool_size=(3,3), strides=(2, 2)))
     model.add(Dropout(0.2))
     # Conv2D layer 2
-    model.add(Convolution2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
     model.add(Dropout(0.2))
     # Conv2D layer 3
-    model.add(Convolution2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+    # Conv2D layer 4
+    model.add(Convolution2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(0.2))
     # Flatten data fro dense layers
     model.add(Flatten())
-    # Dense layer
+    # Dense layer 1
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.4))
-    # Dense layer 2/sigmoid boi
+    # Dense layer 2
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    # Dense layer 3/sigmoid boi
     model.add(Dense(1, activation='sigmoid'))
     # Compile model
+
+    optimizer = optimizers.Adam(lr=0.001, decay=0.0)
     model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
+                  optimizer=optimizer,
                   metrics=['accuracy'])
     model.summary()
     return model
